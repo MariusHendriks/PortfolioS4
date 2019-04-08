4690a33abe6f7a1944da591a2ccde409c769c35a
MariusHendriks <Mark.hendriks9999@gmail.com>
2019-04-08 21:18:38 +0200

    anders snap ik er morgen niks meer van

--- app.js
+++ app.js
@@ -14,11 +14,10 @@ var db = firebase.firestore();
 
 var docRef = db.collection("documents").doc("6CGSwq8mvQuhESTlKUIs");
 
-var UXU = new Map();
-var DEV = new Map();
-var PTM = new Map();
-var SCO = new Map();
-var cars = ["Saab", "Volvo", "BMW"];
+var uxu = [];
+var ded = [];
+var ptm = [];
+var sco = [];
 
 db.collection("documents")
   .get()
@@ -30,13 +29,17 @@ db.collection("documents")
       UXU.set("test", doc.data);
       console.log(doc.id, " => ", doc.data());
       console.log("Pooop ", data);
-      myFunction(data);
+      setArrays(data);
     });
   });
 
-function myFunction(array) {
+function setArrays(array) {
   for (var i = 0; i < array.length; i++) {
     console.log(array[i]);
+    //maak object en neuk die in de db
+    if (array[i].course == "uxu") {
+      uxu.push(array[i].course);
+    }
     console.log(array[i].course);
   }
 }
