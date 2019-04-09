31c0cce0ee2ec03d8334cf2c62e33241126e97c4
MariusHendriks <Mark.hendriks9999@gmail.com>
2019-04-09 20:31:57 +0200

    iframe loads

--- app.js
+++ app.js
@@ -11,35 +11,14 @@ var config = {
 firebase.initializeApp(config);
 
 var db = firebase.firestore();
-
-var docRef = db.collection("documents").doc("6CGSwq8mvQuhESTlKUIs");
-
-var uxu = [];
-var ded = [];
-var ptm = [];
-var sco = [];
+var data = [];
+const doneLoading = false;
 
 db.collection("documents")
   .get()
   .then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-      var data = [];
-      // doc.data() is never undefined for query doc snapshots
       data.push(doc.data());
-      UXU.set("test", doc.data);
-      console.log(doc.id, " => ", doc.data());
-      console.log("Pooop ", data);
-      setArrays(data);
+      doneLoading = true;
     });
   });
-
-function setArrays(array) {
-  for (var i = 0; i < array.length; i++) {
-    console.log(array[i]);
-    //maak object en neuk die in de db
-    if (array[i].course == "uxu") {
-      uxu.push(array[i].course);
-    }
-    console.log(array[i].course);
-  }
-}
